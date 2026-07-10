8f3441ed52f9653de1d622ce1367c347fcad259f
Mikiyas Kebede <mknewkebede@gmail.com>
2026-07-10 14:24:27 +0300

    add horizontal price animation

--- src/components/CurrencyPair.tsx
+++ src/components/CurrencyPair.tsx
@@ -17,12 +17,12 @@ const CurrencyPair = ({
   const positive = rateChange > 0 ? true : false;
 
   return (
-    <div className="py-1 px-5 border-x-2 border-neutral-400 flex items-center gap-3 text-md">
+    <div className="h-10 px-8 border-x-1 border-neutral-400 flex items-center gap-3 text-md">
       {/* currency pairs */}
       <span className="uppercase text-neutral-100">
         {firstCurrency}/{secondCurrency}{" "}
       </span>
-      <span className="uppercase text-white">{price}</span>
+      <span className="uppercase text-white">{price.toPrecision(5)}</span>
       <span
         className={`uppercase ${positive ? "text-green" : "text-red"} flex items-center gap-1`}
       >
@@ -32,7 +32,7 @@ const CurrencyPair = ({
           alt={positive ? "up arrow" : "down arrow"}
           className={`${positive ? "rotate-180" : "rotate-0"} w-5 h-5`}
         />
-        {positive ? `+${rateChange}` : `${rateChange}`}
+        {positive ? `+${rateChange}%` : `${rateChange}%`}
       </span>
     </div>
   );
